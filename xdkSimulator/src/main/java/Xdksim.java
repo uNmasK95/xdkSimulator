583be2050354a97a7a4aac47a023d89bb9f81592
Rui Freitas <rui4825@gmail.com>
2016-09-29 16:20:31 +0100

    2 teste

---
+++ xdkSimulator/src/main/java/Xdksim.java
@@ -0,0 +1,18 @@
+/**
+ * Created by rjaf on 29/09/16.
+ */
+public class Xdksim {
+
+    public static float acelerometro;
+    public static float getGiroscopio;
+    public static float humidade;
+    public static float magnetometro;
+    public static float pressao;
+    public static float temperatura;
+    public static float acustica;
+    public static float luz;
+    public static long lasttimeconsult;
+
+    public static float 
+
+}
--- xdkSimulator/src/main/java/teste.java
+++ xdkSimulator/src/main/java/teste.java
@@ -2,4 +2,5 @@
  * Created by rjaf on 29/09/16.
  */
 public class teste {
+
 }
