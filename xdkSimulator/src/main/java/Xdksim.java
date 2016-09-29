00c78e9a255fb19cfe014f0fcf8de0be6e48eb82
Rui Freitas <rui4825@gmail.com>
2016-09-29 16:34:38 +0100

    xdksim

--- xdkSimulator/src/main/java/Xdksim.java
+++ xdkSimulator/src/main/java/Xdksim.java
@@ -1,10 +1,12 @@
+import java.util.Random;
+
 /**
  * Created by rjaf on 29/09/16.
  */
 public class Xdksim {
 
     public static float acelerometro;
-    public static float getGiroscopio;
+    public static float giroscopio;
     public static float humidade;
     public static float magnetometro;
     public static float pressao;
@@ -13,6 +15,22 @@ public class Xdksim {
     public static float luz;
     public static long lasttimeconsult;
 
-    public static float 
+    public static void getacelerometro(){
+        Random r;
+        r = new Random ();
+        float p = r.nextFloat();
+    }
+
+    public static void gettemperatura(){
+        Random r;
+        r = new Random();
+        temperatura = r.nextInt(30);
+    }
+
+    public static void getacustica(){
+        Random r;
+        r = new Random();
+
+    }
 
 }
