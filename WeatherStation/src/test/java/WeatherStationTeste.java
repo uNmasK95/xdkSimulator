a3b23b1cc4af5d23f61dbc000ab446b874d1683b
Rui Freitas <rui4825@gmail.com>
2016-12-07 16:11:45 +0000

    modificação dos testes

--- WeatherStation/src/test/java/WeatherStationTeste.java
+++ WeatherStation/src/test/java/WeatherStationTeste.java
@@ -2,9 +2,12 @@ import Refatoring.WeatherModel;
 import Refatoring.WeatherStation;
 import Refatoring.WeatherView;
 import Refatoring.XDK;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runners.Parameterized;
 
+import java.io.*;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Vector;
@@ -17,6 +20,19 @@ import static org.junit.Assert.assertEquals;
 public class WeatherStationTeste {
 
     @Parameterized.Parameter
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Parameterized.Parameter
     Vector<Integer> xdk1_reading_1= new Vector<>();
 
     @Parameterized.Parameter
@@ -25,9 +41,18 @@ public class WeatherStationTeste {
     @Parameterized.Parameter
     HashMap<LocalDate, Vector<Integer>> last_values = new HashMap<LocalDate, Vector<Integer>>();
 
+    @Parameterized.Parameter
+    WeatherModel wModel = new WeatherModel();
 
-    @Test
-    public void xdkTesteReading1_2() {
+    @Parameterized.Parameter
+    WeatherStation wStation = new WeatherStation(wModel);
+
+    @Parameterized.Parameter
+    WeatherView wView = new WeatherView(wStation);
+
+
+    @Before
+    public void xdkReading1_2() {
 
         xdk1_reading_1.add(23);
         xdk1_reading_1.add(2);
@@ -41,20 +66,38 @@ public class WeatherStationTeste {
         xdk1_reading_2.add(4);
         xdk1_reading_2.add(5);
 
+        XDK xdk1 = new XDK(wStation,1);
+
+        xdk1.start(xdk1_reading_1);
+        xdk1.start(xdk1_reading_2);
+    }
+
+
+    @Test
+    public void mostraTeste(){
+        wView.mostra_temperatura();
+        assertEquals("Tempertura Actual: 22\n",outContent.toString());
+    }
+
+    @Test
+    public void mostra_max_minimo_temperaturaTeste(){
+        wView.mostra_max_minimo(LocalDate.now(), WeatherModel.idTemperatura);
+        assertEquals("Max temperatura: 23 Min temperatura: 22\n" , outContent.toString());
+    }
+
+/*
+
+    @Test
+    public void xdkTesteReading() {
+
         WeatherStation iot = new WeatherStation();
         XDK xdk1 = new XDK(iot,1);
 
         xdk1.start(xdk1_reading_1);
         xdk1.start(xdk1_reading_2);
 
-        WeatherView view = new WeatherView();
 
-        // assert statements
 
-        assertEquals("Tempertura Actual: 22",iot.mostra_temperatura());
-
-        assertEquals(22, iot.mostra_temperatura());
-        assertEquals("Max temperatura: 23 Min temperatura: 22" , iot.mostra_max_minimo(LocalDate.now(), WeatherModel.idTemperatura) );
         assertEquals("Max humidade: 24 Min humidade: 2"  , iot.mostra_max_minimo(LocalDate.now(), WeatherModel.idHumidade) );
         assertEquals("Max pressão atmosférica: 3 Min pressão atmosférica: 3", iot.mostra_max_minimo(LocalDate.now(), WeatherModel.idPressao_atm) );
 
@@ -73,31 +116,5 @@ public class WeatherStationTeste {
 
         assertEquals(last_values , iot.mostra_ultimos_dias(0,2));
 
-
-    }
-
-
-    @Test
-    public void xdkTesteReading() {
-
-        xdk1_reading_1.add(23);
-        xdk1_reading_1.add(2);
-        xdk1_reading_1.add(3);
-        xdk1_reading_1.add(54);
-        xdk1_reading_1.add(32);
-
-        xdk1_reading_2.add(22);
-        xdk1_reading_2.add(24);
-        xdk1_reading_2.add(3);
-        xdk1_reading_2.add(4);
-        xdk1_reading_2.add(5);
-
-        WeatherStation iot = new WeatherStation();
-        XDK xdk1 = new XDK(iot,1);
-
-        xdk1.start(xdk1_reading_1);
-        xdk1.start(xdk1_reading_2);
-
-
-    }
+    }*/
 }
